aab174d90fe6d78e80cd10a30a392d67743a64ca
1Eugene-dot <eugenepretorius62@gmail.com>
2022-05-18 21:57:59 +0200

    20220518

--- A_bat_watch.ino
+++ A_bat_watch.ino
@@ -6,6 +6,7 @@
 #include <EthernetClient.h>
 #include <Dns.h>
 #include <Dhcp.h>
+#include "EmonLib.h"
 
 /************************* Ethernet Client Setup *****************************/
 byte mac[] = {0xDA, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -14,7 +15,7 @@ byte mac[] = {0xDA, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         "...your AIO key..."
+#define AIO_KEY         "...your AIO password..."
 
 //Set up the ethernet client
 EthernetClient client;
@@ -29,14 +30,33 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 
 // Setup a feed called 'photocell' for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/photocell");
+Adafruit_MQTT_Publish BatData = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/Data");
 
-// Setup a feed called 'onoff' for subscribing to changes.
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+/*****************************Pin Setup **************************************/
+
+int PowerLed = 2; // Power LED
+int Connected = 3; // Connected to MQTT server
+int Run = 4; // Hearbeat
+int DC_in = 0; // voltage div analog in
+int CT1 = 1; //Current clamp 1 analog in
+int CT2 = 2; //Curent clamp 2 analog in
+
+EnergyMonitor emon1;
+
+float CT_correction = 111.1;
+char json_data[200];
+float CT1_old = 0.0;
+float CT2_old = 0.0;
+float DC_old = 0.0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(PowerLed, OUTPUT);
+  digitalWrite(PowerLed, HIGH);
+  pinMode(Connected, OUTPUT);
+  pinMode(Run, OUTPUT);
 
   Ethernet.begin(mac);
   delay(1000); //give the ethernet a second to initialize
@@ -44,22 +64,36 @@ void setup() {
 }
 
 void loop() {
+  StaticJsonDocument<200> doc;
+  delay(1000);
+  digitalWrite(Run, HIGH);
+  doc["CT1"] = getCT(CT1);
+  doc["CT2"] = getCT(CT2);
+  doc["A1"] = getAnalog(DC_in);
   // put your main code here, to run repeatedly:
   MQTT_connect();
-  StaticJsonDocument<200> doc;
-  // Now we can publish stuff!
-  serializeJson(doc, Serial);
-  if (! photocell.publish(Serial)) {
-    Serial.println(F("Failed"));
-  } else {
-    Serial.println(F("OK!"));
-  }
 
+  if (getCT(CT1) != CT1_old || getCT(CT2) != CT2_old || getAnalog(DC_in) != DC_old) {
+    // Now we can publish stuff!
+    serializeJson(doc, json_data);
+    if (! BatData.publish(json_data)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+      CT1_old = getCT(CT1);
+      CT2_old = getCT(CT2);
+      DC_old = getAnalog(DC_in);
+    }
+  }
   // ping the server to keep the mqtt connection alive
   if (! mqtt.ping()) {
+    digitalWrite(Connected, LOW);
     mqtt.disconnect();
   }
 
+
+  delay(1000);
+  digitalWrite(Run, LOW);
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -69,6 +103,7 @@ void MQTT_connect() {
 
   // Stop if already connected.
   if (mqtt.connected()) {
+    digitalWrite(Connected, HIGH);
     return;
   }
 
@@ -76,9 +111,28 @@ void MQTT_connect() {
 
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
     Serial.println(mqtt.connectErrorString(ret));
+    digitalWrite(Connected, LOW);
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
     delay(5000);  // wait 5 seconds
   }
   Serial.println("MQTT Connected!");
+}
+
+float getCT(int pin) {
+  emon1.current(pin, CT_correction);
+  delay(500);
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  Serial.print(Irms * 230.0);         // Apparent power
+  Serial.print(" ");
+  Serial.println(Irms);
+  return (Irms * 230.0);
+}
+
+float getAnalog(int pin) {
+  float pin_data = analogRead(pin);
+
+  float return_data = (pin_data / 5.00) * 100;
+
+  return return_data;
 }
